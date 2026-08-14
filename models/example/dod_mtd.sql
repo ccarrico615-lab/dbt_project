5828ff1efd51c9b6fdebf30489276f6a548d1ff2
ccarrico615@gmail.com <ccarrico615@gmail.com>
2026-08-14 19:10:13 +0000

    Create mtd and wtd models

---
+++ models/example/dod_mtd.sql
@@ -0,0 +1,37 @@
+with dates as (
+    select date, wk_no, mth_no
+    from {{ source('dod_raw', 'dim_date') }}
+),
+
+workouts as (
+    select date, sum(session_cnt) as session_cnt, sum(duration_minutes) as duration_minutes, sum(vol_lbs) as vol_lbs
+    from {{ source('dod_raw', 'dod_workout') }}
+    group by date
+),
+
+steps as (
+    select date, step_tot
+    from {{ source('dod_raw', 'dod_steps') }}
+)
+
+SELECT  date, mth_no, eom_ind,        
+        mtd_session_cnt, mtd_vol_lbs, mtd_step_tot
+FROM
+(
+    SELECT
+        dt.date,
+        dt.mth_no,
+        sum(wo.session_cnt) over(partition by dt.mth_no order by dt.date) as mtd_session_cnt,
+        sum(wo.vol_lbs) over(partition by dt.mth_no order by dt.date) as mtd_vol_lbs,
+        sum(stp.step_tot) over(partition by dt.mth_no order by dt.date) as mtd_step_tot,
+        case when
+            dt.mth_no = lead(dt.mth_no,1) over(order by dt.date) then 0
+            else 1
+        end as eom_ind
+    FROM
+        dates as dt
+        LEFT JOIN workouts as wo
+            ON dt.date = wo.date
+        LEFT JOIN  steps as stp
+            ON dt.date = stp.date
+)
