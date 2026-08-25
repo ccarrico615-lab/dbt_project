1b006108a055158e2c82a42a36113cd12eb603f3
Christopher Carrico <ccarrico615@gmail.com>
2026-08-25 14:57:50 +0000

    Additional fixes to schema

--- models/BizzyCar/intermediate/vehicle_dol.sql
+++ models/BizzyCar/intermediate/vehicle_dol.sql
@@ -1,14 +1,16 @@
 SELECT    
     veh.vehicle_id,
     sale.sale_id, --it's possible one of these vehicles could sell multiple times. Pull this in to join on later
-    case when sale.sale_id is null then "Available" else "Sold" end as sale_status,
+    case when sale.sale_id is null then 'Available' else 'Sold' end as sale_status,
     veh.make,
     veh.model,
     veh.year,
     veh.vin,
     veh.mileage,
     veh.date_listed,
-    date_diff(coalesce(sale.date_sold,current_date), veh.date_listed, day) as days_on_lot, -- grab the difference between date_listed and current date in case user wants to see how long the car has been sitting unsold
+    -- grab the difference between date_listed and current date in case user wants to see how long the car has been sitting unsold
+    -- it does mean this value will change depending on when the model is run. I would typically base this on some sort of snapshot date instead of "current_date"
+    date_diff(coalesce(sale.date_sold,current_date), veh.date_listed, day) as days_on_lot, 
     veh.dealership_id
 FROM
     {{ ref('stg_vehicle') }} veh
